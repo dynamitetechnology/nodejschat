3093aab90b8510eefd8764003af545ee18dec039
technologydynamite@gmail.com <suraj.cartgeek@gmail.com>
2021-08-19 01:16:38 +0530

    design

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const path = require('path')
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
     //res.send('<h1>Hello world</h1>');
 });
 
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 
   io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+    socket.on('chat message', (msg,username) => {
+      io.emit('chat message', msg,username);
     });
   });
 
